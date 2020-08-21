9d7bb87cdcc3e892800a69d78a07f4a213f476c9
Bikesh <Biku@Bikeshs-MacBook-Air-2.local>
2020-08-21 08:41:57 -0700

    created axioswithauth and privateroute now working on login form

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,6 +1,11 @@
 import React from "react";
+import axios from 'axios'
 
 const Login = () => {
+  axios.post()
+  .then()
+  .catch()
+
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
   return (
